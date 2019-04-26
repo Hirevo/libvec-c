42c3fa5b07c80de960eb2c8e213a581a9c259ab8
Nicolas Polomack <nicolas@polomack.eu>
2019-04-26 02:57:56 +0200

    Fixed clang warning

--- src/lvec_last_index_of.c
+++ src/lvec_last_index_of.c
@@ -13,6 +13,7 @@ ssize_t lvec_last_index_of(vec_t *this, void *elem)
 
 	if (this == 0 || this->size == 0)
 		return (-1);
-	for (idx = this->size - 1; idx >= 0 && this->arr[idx] != elem; idx--);
+	for (idx = this->size - 1; idx >= 0 && this->arr[idx] != elem; idx--)
+        ;
     return (idx);
 }
